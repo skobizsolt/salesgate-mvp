0ae47f2eb8f56129a679470e58a54dd4013d0abb
Erik Zsolt Skobrák <skobizsolt@gmail.com>
2024-07-04 18:44:43 +0200

    feat: content save API changes

--- src/main/java/com/upscale/salesgate/service/DefaultContentsService.java
+++ src/main/java/com/upscale/salesgate/service/DefaultContentsService.java
@@ -3,16 +3,16 @@ package com.upscale.salesgate.service;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.upscale.salesgate.exception.Errors;
 import com.upscale.salesgate.exception.ServiceExpection;
-import com.upscale.salesgate.model.GetContentResponse;
-import com.upscale.salesgate.model.PostContentRequest;
-import com.upscale.salesgate.model.TemplateField;
+import com.upscale.salesgate.model.*;
 import com.upscale.salesgate.persistence.ContentsRepository;
 import com.upscale.salesgate.service.dto.ContentDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DefaultContentsService implements ContentsService {
 
     private final TemplatesService templatesService;
@@ -21,17 +21,8 @@ public class DefaultContentsService implements ContentsService {
     @Override
     public void saveContent(final String templateId, final PostContentRequest request) {
         final var template = templatesService.getTemplate(templateId);
-
-        final TemplateField field = template.getFields()
-                .stream()
-                .filter(templateField -> templateField.getName().equals(request.getName()))
-                .findFirst()
-                .orElseThrow(() -> new ServiceExpection(Errors.TEMPLATE_ERROR));
-        final ContentDto contentDto = ContentDto.builder()
-                .name(request.getName())
-                .data(convert(request.getData(), field.getType()))
-                .build();
-        contentsRepository.saveContent(templateId, contentDto);
+        request.getContentData()
+                .forEach(dataRecord -> saveRecord(template, templateId, dataRecord));
     }
 
     @Override
@@ -44,8 +35,25 @@ public class DefaultContentsService implements ContentsService {
                 );
     }
 
+    private void saveRecord(final GetTemplateResponse template, final String templateId, final DataRecord dataRecord) {
+        final TemplateField field = template.getFields()
+                .stream()
+                .filter(templateField -> templateField.getName().equals(dataRecord.getName()))
+                .findFirst()
+                .orElse(null);
+        if (field == null) {
+            log.info("Field '{}' is not found in this template, skipping...", dataRecord.getName());
+            return;
+        }
+        final ContentDto contentDto = ContentDto.builder()
+                .name(dataRecord.getName())
+                .data(convert(dataRecord.getData(), field.getType()))
+                .build();
+        contentsRepository.saveContent(templateId, contentDto);
+    }
+
     @SuppressWarnings("java:S1905")
-    private Object convert(final String data, final TemplateField.TypeEnum type) {
+    private static Object convert(final String data, final TemplateField.TypeEnum type) {
         return switch (type) {
             case STRING -> (String) data;
             case INTEGER -> Integer.parseInt((String) data);
